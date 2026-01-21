7cac5d76c68a9303c9982d185f3c19f44d9276aa
martinsllm <lucaslm2010@gmail.com>
2026-01-20 21:22:46 -0300

    Grouping routes

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SeriesController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('series.index');
 });
 
-Route::get('/series', [SeriesController::class, 'index'])->name('series.index');
-Route::get('/series/create', [SeriesController::class, 'create'])->name('series.create');
-Route::post('/series/insert', [SeriesController::class, 'store'])->name('series.store');
+Route::resource('/series', SeriesController::class);
