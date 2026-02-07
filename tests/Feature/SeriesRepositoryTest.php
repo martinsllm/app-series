e9bb1105d72c8f71a1fbb8ebde5b8d86c9049744
martinsllm <lucaslm2010@gmail.com>
2026-02-07 17:20:12 -0300

    Creating feature tests for Series

--- tests/Feature/SeriesRepositoryTest.php
+++ tests/Feature/SeriesRepositoryTest.php
@@ -50,4 +50,57 @@ class SeriesRepositoryTest extends TestCase
         $this->assertDatabaseCount('episodes', 12);
 
     }
+
+    public function test_when_all_series_are_retrieved(): void
+    {
+        $repository = $this->app->make(SeriesRepository::class);
+        $request1 = [
+            'name' => 'Série Teste 1',
+            'seasonsQty' => 2,
+            'episodesForSeason' => 3,
+        ];
+
+        $request2 = [
+            'name' => 'Série Teste 2',
+            'seasonsQty' => 1,
+            'episodesForSeason' => 4,
+        ];
+
+        $repository->add($request1);
+        $repository->add($request2);
+
+        $series = $repository->all();
+
+        $this->assertCount(2, $series);
+        $this->assertEquals('Série Teste 1', $series[0]['name']);
+        $this->assertEquals('Série Teste 2', $series[1]['name']);
+        $this->assertIsArray($series);
+    }
+
+    public function test_when_a_series_is_retrieved_by_id(): void
+    {
+        $repository = $this->app->make(SeriesRepository::class);
+        $request = [
+            'name' => 'Série Teste',
+            'seasonsQty' => 2,
+            'episodesForSeason' => 3,
+        ];
+
+        $series = $repository->add($request);
+
+        $seriesById = $repository->find($series->id);
+
+        $this->assertEquals('Série Teste', $seriesById['name']);
+    }
+
+    public function test_when_a_non_existent_series_is_retrieved_by_id(): void
+    {
+        $repository = $this->app->make(SeriesRepository::class);
+
+        $this->expectException(\Illuminate\Database\Eloquent\ModelNotFoundException::class);
+
+        $repository->find(999);
+    }
+
+    
 }
