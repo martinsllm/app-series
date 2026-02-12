38a3dfa560719261df2695ef830c9eb71572551b
martinsllm <lucaslm2010@gmail.com>
2026-02-12 19:45:53 -0300

    Casts for boolean in table episodios

--- app/Models/Episode.php
+++ app/Models/Episode.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
@@ -10,9 +11,14 @@ class Episode extends Model
     use HasFactory;
     protected $fillable = ['number'];
     public $timestamps = false;
+    protected $casts = [
+        'watched' => 'boolean',
+    ];
 
     public function season()
     {
         return $this->belongsTo(Season::class);
     }
+
+    
 }
